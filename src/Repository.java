a5bcac2f828d991b4d7a38e34500aedf026e1199
KoroshRH <46434958+KoroshRH@users.noreply.github.com>
2019-03-17 10:50:35 +0330

    Defining of Repository class is done.

---
+++ src/Repository.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+public class Repository {
+    private int id;
+    private int capacity;
+    private int freeCapacity;
+    private HashMap<Good, Integer> goods;
+
+    public Repository(int id, int capacity){
+        this.id = id;
+        this.capacity = capacity;
+        freeCapacity = capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getFreeCapacity() {
+        return freeCapacity;
+    }
+
+    public HashMap<Good, Integer> getGoods() {
+        return goods;
+    }
+
+    public void addGood(Good g, int amount){
+        if (goods.containsKey(g)){
+            goods.replace(g, goods.get(g)+amount);
+        } else {
+            goods.put(g, amount);
+        }
+    }
+
+    public void removeGood(Good g, int amount){
+        goods.replace(g, goods.get(g)-amount);
+    }
+}
